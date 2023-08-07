b069376b407917803f82f8da50bce977710c64ca
Nikita Valovyy <nickwolf046@gmail.com>
2023-08-07 16:02:51 -0400

    cart is working

--- routes/cart.js
+++ routes/cart.js
@@ -7,47 +7,41 @@ const User = require("../models/User");
 const isAuthenticated = require("../middleware/isAuthenticated");
 
 router.get("/", isAuthenticated, (req, res, next) => {
-  const cartId = req.user.cart;
 
-  Cart.findById(cartId)
-    .populate("items")
-        .then((foundItems) => {
-          if (!foundItems) {
-            return res.json({ message: "Your cart is empty" });
-          }
-          console.log("found:", foundItems)
-          res.json(foundItems);
-        })
-        .catch((err) => {
-          console.log(err);
-          next(err);
-        });
-});
+        Cart.findOne({
+          owner: req.user._id
+      })
+          .populate('items')
+          .then((foundCart) => {
+              if(!foundCart) {
+                  return res.json({message: 'Your cart is empty'})
+              }
+              res.json(foundCart)
+          })
+          .catch((err) => {
+              console.log(err)
+              next(err)
+          })
+  
+  });
+
+
+
+
+
+
+
 
 router.post("/create", isAuthenticated, async (req, res, next) => {
   try {
-    const details = req.body;
+    const details = req.body.details;
     console.log("Item:", details);
 
     const createdCart = await Cart.create({
       owner: req.user._id,
       subtotal: details.cost,
       total: Math.floor(details.cost * 1.08),
-
     });
-
-    const updatedUser = await User.findByIdAndUpdate(
-      req.user._id,
-      {
-        $push: { cart: createdCart._id }, 
-      },
-      { new: true }
-    );
-
-    const populatedUser = await updatedUser.populate({
-      path: 'cart', 
-      // populate: {path: 'items owner'}
-    })
 
     createdCart.items.push(details._id);
     createdCart.save();
@@ -55,8 +49,8 @@ router.post("/create", isAuthenticated, async (req, res, next) => {
     const populatedCart = await createdCart.populate('items')
 
     console.log("Created cart:", populatedCart);
-    console.log("Updated user:", populatedUser);
-    res.json({createdCart, populatedUser});
+
+    res.json(createdCart);
   } catch (err) {
     console.log(err);
     next(err);
@@ -64,21 +58,23 @@ router.post("/create", isAuthenticated, async (req, res, next) => {
 });
 
 router.post("/update", isAuthenticated, (req, res, next) => {
-  const details = req.body;
-
-  const cartId = req.user.cart;
+  const {details, cartId} = req.body;
+  console.log("details:",details);
+  console.log("cart ID:",cartId);
+  console.log("Req.body:",req.body);
 
   Cart.findByIdAndUpdate(
     cartId,
     {
-      subtotal,
-      total,
+      // subtotal,
+      // total,
       $push: { items: details._id },
     },
     { new: true }
   )
     .populate("items")
     .then((updatedCart) => {
+      console.log("updated Cart:",updatedCart)
       res.json(updatedCart);
     })
     .catch((err) => {
